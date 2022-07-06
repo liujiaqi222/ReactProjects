16b1ba61593e406d4b526bcb251e1945f7f28ce9
liujiaqi222 <2473487465@qq.com>
2022-07-06 23:33:30 +0800

    update

--- tiktik/components/Sidebar.tsx
+++ tiktik/components/Sidebar.tsx
@@ -13,12 +13,12 @@ const Sidebar = () => {
   const userProfile = false; //用户没有登陆
   const normalLink = 'flex items-center justify-center gap-3 hover:bg-primary p-3  xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded'
   return (
-    <div>
+    <div className=' overflow-x-hidden'>
       <div className='block xl:hidden m-2 ml-4 mt-3 text-xl' onClick={() => setShowSidebar((prev) => !prev)}>
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className='xl:w-400 xl:border-0 p-3 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100'>
+        <div className='xl:w-400 xl:border-0 p-3 w-20 flex flex-col justify-start mb-0 border-r-2 border-gray-100'>
           <div className='xl:border-b-2 xl:pb-4 border-gray-200'>
             <Link href='/'>
               <div className={normalLink}>
