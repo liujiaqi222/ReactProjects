50ac70c5735747708a368b26acf0bbc1b693b5a8
liujiaqi222 <2473487465@qq.com>
2022-07-06 22:50:10 +0800

    update

---
+++ tiktik/components/Discover.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import {topics} from '../utils/constants'
+
+
+function Discover() {
+  return (
+    <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
+      <p></p>
+    </div>
+  )
+}
+
+export default Discover
